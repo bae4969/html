3e065f5ee818969c45968e4adec1bd34ec2e0347
bae4969 <bae4969@naver.com>
2021-05-25 09:11:15 +0900

    added maintenance page

---
+++ maintenance.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <title>template</title>
+    <link rel="stylesheet" href="css/outer.css">
+    <style>
+        div#output1{
+            position: absolute;
+            top: 40%;
+            left: 41%;
+            font-size: 3em;
+            display: block;
+        }
+        div#output2{
+            position: absolute;
+            top: 48%;
+            left: 37%;
+            font-size: 3em;
+            display: block;
+        }
+    </style>
+</head>
+
+<body>
+    <div id=output1>
+        시스템 점검중...
+    </div>
+    <div id=output2>
+        System Maintenance...
+    </div>
+</body>
+
+</html>
