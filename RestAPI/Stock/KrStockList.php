403598c1de22f337c7f19b84152174903301e6cf
bae4969 <bae4969@naver.com>
2024-07-16 22:39:08 +0900

    다른 포트로 주식 rest api 중 기본 요청 생성

---
+++ RestAPI/Stock/KrStockList.php
@@ -0,0 +1,17 @@
+<?php
+
+include '/var/www/php/sql_connection_info.php';
+
+$conn = mysqli_connect($sqlAddr, $sqlId, $sqlPw);
+$conn->set_charset("utf8mb4");
+$sql_query = 'SELECT stock_code, stock_name_kr FROM KoreaInvest.stock_info WHERE stock_market="KOSPI" OR stock_market="KOSDAQ"';
+if($_GET["count"])
+	$sql_query .= $_GET["count"];
+$result = mysqli_query($conn, $sql_query);
+mysqli_close($conn);
+
+$stock_list = array();
+while ($row = mysqli_fetch_assoc($result))
+	$stock_list[] = $row;
+
+echo json_encode($stock_list, JSON_UNESCAPED_UNICODE);
